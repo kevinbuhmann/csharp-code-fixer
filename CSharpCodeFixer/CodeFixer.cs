03e63990773abd7e48cfe49a8cc7826fe58b23ba
kevinphelps <KevinPhelps11@gmail.com>
2016-10-12 23:38:40 -0500

    fix for SA1005

--- CSharpCodeFixer/CodeFixer.cs
+++ CSharpCodeFixer/CodeFixer.cs
@@ -11,6 +11,7 @@ namespace CSharpCodeFixer
         public static void FixAll(string filePath)
         {
             FixSA1101PrefixLocallCallsWithThis(filePath);
+            FixSA1005SingleLineCommentMustBeginWithASpace(filePath);
         }
 
         public static void FixSA1101PrefixLocallCallsWithThis(string filePath)
@@ -27,6 +28,25 @@ namespace CSharpCodeFixer
                 string codeBeforeViolation = code.Substring(0, diagnostic.Location.SourceSpan.Start);
                 string codeAfterViolation = code.Substring(diagnostic.Location.SourceSpan.Start);
                 code = $"{codeBeforeViolation}this.{codeAfterViolation}";
+            }
+
+            File.WriteAllText(filePath, code, Encoding.UTF8);
+        }
+
+        public static void FixSA1005SingleLineCommentMustBeginWithASpace(string filePath)
+        {
+            string code = File.ReadAllText(filePath);
+
+            IEnumerable<Diagnostic> diagnostics = Analyzer.GetDiagnostics(filePath, code)
+                .Where(diagnostic => diagnostic.Id == "SA1005")
+                .OrderByDescending(diagnostic => diagnostic.Location.SourceSpan.Start)
+                .ToList();
+
+            foreach (Diagnostic diagnostic in diagnostics)
+            {
+                string codeBeforeViolation = code.Substring(0, diagnostic.Location.SourceSpan.Start + 2);
+                string codeAfterViolation = code.Substring(diagnostic.Location.SourceSpan.Start + 2);
+                code = $"{codeBeforeViolation} {codeAfterViolation}";
             }
 
             File.WriteAllText(filePath, code, Encoding.UTF8);
